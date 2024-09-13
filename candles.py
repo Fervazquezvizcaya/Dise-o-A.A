536188a4cba5bee56666cf4b5f375485fe17c44b
fer <fernando_vv11@outlook.com>
2024-09-12 22:37:11 -0600

    cambie comentarios

--- candles.py
+++ candles.py
@@ -1,24 +1,25 @@
-def candles(a: list[int]) -> int:
+
+def candles(height: list[int]) -> int:
     """
     Args:
-        a (list[int]): The candles heights.
+        height (list[int]): The candles heights.
     
     Returns:
         int: The number of candles that are tallest
     """
-
-def candles(height: list[int]) -> int:
-    
     
     max_height = max(height)  # Encuentra la altura máxima
     count_max_height = 0      # Inicializa el contador
     
-    # Ciclo que cuenta cuántas veces aparece la altura máxima
+    # Ciclo que cuenta cuántas veces aparece la altura máxima, igual se puede usar un counter y nos ahorramos la estructura four pero ocupare el four para poderce implementar mejor en pseuodocodigo y Diagrama F.
     for h in height:
         if h == max_height:
             count_max_height += 1
     
+    # count_max_height = height.count(max_height): esta linea sustituiria el ciclo for 
+    
     return count_max_height  # Regresa el contador
+
 
 
 
